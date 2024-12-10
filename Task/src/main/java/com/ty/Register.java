3c5bd40d1661a18dbf7870c20a4439b3fecb090d
Nishant Sanap <144839538+nishantsanap@users.noreply.github.com>
2024-12-10 23:39:26 +0530

    Update Register.java

--- Task/src/main/java/com/ty/Register.java
+++ Task/src/main/java/com/ty/Register.java
@@ -1,49 +1,3 @@
-//package com.ty;
-//
-//import java.io.IOException;
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.PreparedStatement;
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//@WebServlet("/register")
-//public class Register extends HttpServlet {
-//	String url = "jdbc:postgresql://localhost:5432/task?user=postgres&password=root";
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//			throws ServletException, IOException {
-//		String name = request.getParameter("name");
-//		String email = request.getParameter("email");
-//		String phoneNo = request.getParameter("phone_no");
-//		String password = request.getParameter("password");
-//
-//		try {
-//			Class.forName("org.postgresql.Driver");
-//			Connection conn = DriverManager.getConnection(url);
-//			PreparedStatement stmt = conn.prepareStatement("INSERT INTO users (name, email, phone_no, password) VALUES (?, ?, ?, ?)");
-//
-//			stmt.setString(1, name);
-//			stmt.setString(2, email);
-//			stmt.setString(3, phoneNo);
-//			stmt.setString(4, password);
-//			stmt.executeUpdate();
-//			stmt.close();
-//			conn.close();
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		response.sendRedirect("login.html");
-//
-//		// request.setAttribute("phone_no", phoneNo);
-//		// request.setAttribute("password", password);
-//		// request.getRequestDispatcher("login").forward(request, response);
-//	}
-//}
-
 package com.ty;
 
 import java.io.IOException;
@@ -84,11 +38,9 @@ public class Register extends HttpServlet {
 			e.printStackTrace();
 		}
 		response.sendRedirect("login.html");
-
-
-
 	}
 }
+
 
 
 
